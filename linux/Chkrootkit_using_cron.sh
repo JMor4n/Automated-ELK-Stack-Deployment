0473c2e8b409e6f59905db6380637639b9fb0233
Juan Moran <5645398+JMor4n@users.noreply.github.com>
2021-06-18 22:09:29 +1000

    Update Chkrootkit_using_cron.sh

--- linux/Chkrootkit_using_cron.sh
+++ linux/Chkrootkit_using_cron.sh
@@ -1,4 +1,5 @@
-h1. As a system administrator an script can be created to run chkrootkit and email the result using a cron job. The following script show how to do achive this.
+### As a system administrator an script can be created to run chkrootkit and email the result using a cron job. The following script show how to do achive this.
+---
 
 #!/bin/bash
 echo “If you feel chkrootkit is outdated, update it manually ;)” > /tmp/chkrootkit
