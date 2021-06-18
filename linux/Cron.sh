22c67c973f858893aaf5857f7b1fa4c18bcbfbd1
Juan Moran <5645398+JMor4n@users.noreply.github.com>
2021-06-18 22:11:52 +1000

    Delete Cron.sh

--- linux/Cron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo “If you feel chkrootkit is outdated, update it manually ;)” > /tmp/chkrootkit
-echo ” ” >> /tmp/chkrootkit
-echo “Scanning the system with ChkRootkit” >> /tmp/chkrootkit/usr/local/chkrootkit/chkrootkit >> /tmp/chkrootkit
-cat /tmp/chkrootkit | mail -s “ChkRootkit scan report on `hostname`” <email address>
